60dc020dd9f602ba61896a91f50833f0b895ce4c
Juanf <juanfa107@gmail.com>
2019-09-29 08:46:53 -0500

    cambios

--- acciones.php
+++ acciones.php
@@ -3,12 +3,12 @@
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
 
-  require_once("Conectar.php");
+ 	require_once("Conectar.php");
 	require_once("funciones_generales.php");
 
 	function conexion(){
 		return 1;
-  }
+  	}
   
 	function iniciarSesion(){
 		$db = new Bd();
@@ -21,9 +21,9 @@
 		return json_encode($sql);
 	}
 
-  function registro(){
+    function registro(){
 
-    $db = new Bd();
+		$db = new Bd();
 		$db->conectar();
 
 		$db->sentencia("INSERT INTO usuarios (nombres, apellidos, email, pass, pin) VALUES (:nombres, :apellidos, :email, :pass, :pin)  ", array(":nombres" => $_REQUEST["nombres"], ":apellidos" => $_REQUEST["apellidos"], ":email" => $_REQUEST["email"], ":pass" => encriptarPass($_REQUEST["pass"]), ":pin"=> "0000"));
@@ -31,11 +31,19 @@
 		$db->desconectar();
 		
 		return 1;
-  }
 
-  function AgregarAhorro(){
-    
-  }
+	}
+
+   function AgregarAhorro(){
+	$db = new Bd();
+	$db->conectar();
+
+	$db->sentencia("INSERT INTO ahorros (fk_id_usuario, nombre, objetivo, ahorrado, intervalo) VALUES (:fk_id_usuario, :nombre, :objetivo, :ahorrado, :intervalo)  ", array(":fk_id_usuario" => $_REQUEST["idUsuario"], ":nombre" => $_REQUEST["nombre"], ":objetivo" => $_REQUEST["objetivo"], ":ahorrado" => $_REQUEST["ahorrado"], ":intervalo" => $_REQUEST["intervalo"]));
+
+	$db->desconectar();
+	
+	return 1;
+   }
 
   if(@$_REQUEST['accion']){
     if(function_exists($_REQUEST['accion'])){
